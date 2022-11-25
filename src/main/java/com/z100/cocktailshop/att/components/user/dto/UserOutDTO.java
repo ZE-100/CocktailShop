637acb9ebe86f18de8976bb2ff15e584214d7dac
Marvin <marvin131t@gmail.com>
2022-11-25 10:59:17 +0100

    #1 Add user base services

---
+++ src/main/java/com/z100/cocktailshop/att/components/user/dto/UserOutDTO.java
@@ -0,0 +1,15 @@
+package com.z100.cocktailshop.att.components.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserOutDTO {
+
+	private Long id;
+
+	private String username;
+
+	private String preferences; //TODO Replace by preferences
+}
