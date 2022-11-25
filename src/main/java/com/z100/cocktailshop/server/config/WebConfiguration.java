a7cd30cb9b486023efa9d70e2c599b8410294898
Marvin <marvin131t@gmail.com>
2022-11-25 11:54:46 +0100

    Add webconfig for cors mapping

---
+++ src/main/java/com/z100/cocktailshop/server/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.z100.cocktailshop.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+				.allowedOriginPatterns("*").allowedHeaders("Authorization", "Cache-Control", "Content-Type")
+				.maxAge(3600);
+	}
+}
