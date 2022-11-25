cd996365d6b5bdfe54a885cd9f2d2302c254583b
Marvin <marvin131t@gmail.com>
2022-11-25 09:29:49 +0100

    initial commit

---
+++ src/main/java/com/z100/cocktailshop/CocktailShopApplication.java
@@ -0,0 +1,12 @@
+package com.z100.cocktailshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailShopApplication.class, args);
+	}
+}
---
+++ src/main/java/com/z100/cocktailshop/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.z100.cocktailshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		model.addAttribute("name", name);
+
+		return "greeting";
+	}
+}
