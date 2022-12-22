8f01f7873834fe651f7f352236e90a44bdb6bb87
Marvin <marvin131t@gmail.com>
2022-12-22 16:42:40 +0100

    fixed user endpoint

--- src/main/java/com/z100/cocktailshop/components/user/service/crud/IUserService.java
+++ src/main/java/com/z100/cocktailshop/components/user/service/crud/IUserService.java
@@ -1,20 +1,21 @@
 package com.z100.cocktailshop.components.user.service.crud;
 
-import com.z100.cocktailshop.components.user.dto.UserChangeInDTO;
 import com.z100.cocktailshop.components.user.dto.UserInDTO;
 import com.z100.cocktailshop.components.user.dto.UserOutDTO;
 import com.z100.cocktailshop.exceptions.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface IUserService {
 
-	@Transactional(rollbackOn = ApiException.class)
 	UserOutDTO register(UserInDTO user);
 
-	@Transactional(rollbackOn = ApiException.class)
-	Boolean update(UserChangeInDTO user);
+	UserOutDTO get(Long id);
 
-	@Transactional(rollbackOn = ApiException.class)
-	Boolean delete(String username);
+	List<UserOutDTO> getAll();
+
+	UserOutDTO update(UserInDTO user);
+
+	Boolean delete(Long id);
 }
