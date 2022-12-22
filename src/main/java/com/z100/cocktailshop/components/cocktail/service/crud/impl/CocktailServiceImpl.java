f41691ef0658c79f10d001ba025814ca9bfb7d8e
Marvin <marvin131t@gmail.com>
2022-12-22 16:41:03 +0100

    fixed cocktail endpoint

--- src/main/java/com/z100/cocktailshop/components/cocktail/service/crud/impl/CocktailServiceImpl.java
+++ src/main/java/com/z100/cocktailshop/components/cocktail/service/crud/impl/CocktailServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Component
 @AllArgsConstructor
@@ -48,10 +49,7 @@ public class CocktailServiceImpl implements CocktailService {
 	@Override
 	public List<CocktailOutDTO> getAllFrom(String username) {
 
-		List<Cocktail> cocktails = cocktailRepository.findAllByUserUsername(username)
-				.orElseThrow(() -> new ApiException("Cocktail with ID not found"));
-
-		return cocktails.stream()
+		return StreamSupport.stream(cocktailRepository.findAll().spliterator(), false)
 				.map(cocktailMapper::entityToOutDTO)
 				.toList();
 	}
